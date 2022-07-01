bface4598604fdca7b3fada475681d4d4f926cbf
Harith Fernando <harithfernan96@gmail.com>
2022-07-01 17:58:54 +0530

    initiate project

---
+++ spring-book-seller/src/test/java/com/harith/springbookseller/SpringBookSellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harith.springbookseller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBookSellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
